38c95bccd0aa627db675fcaf5181b74609e35357
yunea <code.experience01@gmail.com>
2022-03-17 09:02:27 +0100

    fini

---
+++ air14.py
@@ -0,0 +1,7 @@
+#Air OK : fin de l'epreuve de l'air
+#Exercice 13 de l'epreuve de l'air : air14.py
+import random
+
+tab = ["facile", "faisable", "surmontable"]
+i = random.randint(0, 2)
+print("J'ai terminé l'épreuve de l'air et c'était "+tab[i]+".")
